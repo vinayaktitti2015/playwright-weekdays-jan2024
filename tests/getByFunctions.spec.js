5985944ccb34037c0d189e75e52fe4ea12acaa8f
Vinayak <vinayak.prime@gmail.com>
2024-02-08 18:55:21 +0000

    Added getbyfuctions specs

---
+++ tests/getByFunctions.spec.js
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+
+test("getByPlaceholder usecase", async ({ page }) => {
+  await page.goto(
+    "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+  );
+  await page.getByPlaceholder("Username").fill("Admin");
+  await page.getByPlaceholder("Password").fill("admin123");
+  await page.getByRole("button", { name: "Login" }).last().click();
+
+  await expect(page).toHaveURL(
+    "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+  );
+  await expect(page.getByText("Dashboard").last()).toBeVisible();
+});
+
+test("handle nth element", async ({ page }) => {
+  await page.goto("https://the-internet.herokuapp.com/");
+
+  await page.locator("ul li a").nth(5).click();
+
+  await expect(page).toHaveURL("https://the-internet.herokuapp.com/checkboxes");
+});
+
+test("getByTestID usecase", async ({ page }) => {
+  await page.goto("https://www.iproperty.com.my/");
+
+  try {
+    await page.getByRole("button", { name: "Consent" }).click();
+    await page.locator(".ab-close-button").click();
+  } catch (error) {
+    console.warn(error);
+  }
+
+  await page.waitForSelector(page.getByTestId("select-area")) // explicit wait
+  await page.getByTestId("select-area").selectOption("Kuala Lumpur");
+});
+
+test.afterEach(async ({ page }) => {
+  await page.close();
+});
---
+++ tests/softAssertions.spec.js
@@ -0,0 +1,71 @@
+import { test, expect } from "@playwright/test";
+
+// test suite
+test.describe("globalsqa page test", () => {
+  // hooks or annotations
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/samplepagetest/");
+
+    try {
+      await page.waitForTimeout(3000); // static timeout
+      await page
+        .locator("button[aria-label='Consent'] p[class='fc-button-label']")
+        .click();
+    } catch (e) {
+      console.warn("Popup not exist on page");
+    }
+  });
+
+  test("should fill the form successfully", async ({ page }) => {
+    // upload file
+    await page
+      .locator('[name="file-553"]')
+      .setInputFiles("./testdata/sample.pdf");
+
+    // input data
+    await expect.soft(page.locator('[name$="invalid"]')).toBeVisible();
+    await page.locator('[name$="name"]').fill("james doe");
+    await page.locator('[name$="email"]').fill("james@yahoo.com");
+    await page
+      .locator('[name$="website"]')
+      .fill("http://localhost:8080/login?from=%2F");
+
+    // dropdown handling
+    await page.locator("#g2599-experienceinyears").selectOption("7-10");
+
+    // checkbox and radio buttons
+    await page.locator("input[value='Automation Testing']").check();
+    await page.locator("input[value='Post Graduate']").check();
+
+    // alerts handling
+    await page.locator("button[onclick='myFunction()']").click();
+
+    page.on("dialog", async (popup) => {
+      // predefined and user-defined
+      popup.accept();
+      popup.accept();
+    });
+
+    // comments
+    await expect.soft(page.locator("#contact-invalid")).toBeEditable();
+    await page
+      .locator("#contact-form-comment-g2599-comment")
+      .fill("test comments");
+    await page.locator("button[type='submit']").click();
+
+    // results page
+
+    await page.waitForLoadState("domcontentloaded");
+    await expect(page).toHaveTitle("Sample Page Test - GlobalSQA");
+    const message = await page
+      .locator("div[class='content_bgr'] h3:nth-child(1)")
+      .textContent();
+    expect(message).toEqual("Message Sent (go back)");
+  });
+});
+
+/**
+ * test suite - collection of tests
+ * hooks - pre-conditions and post-conditions
+ * test case - tests to be executed
+ */
